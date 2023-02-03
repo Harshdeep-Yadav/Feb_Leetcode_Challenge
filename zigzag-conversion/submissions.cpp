ff7986260972d5ccca3a987b70c240bbdb1b51f5
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-02-03 21:14:37 +0530

    Solved

---
+++ zigzag-conversion/submissions.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        // if(numRows==1){
+        //     return s;
+        // }
+        // string res;
+        // int cycle=numRows*2-2;
+        // for(int i=0;i<s.size();i+=cycle){
+        //     res.push_back(s[i]);
+        // }
+        vector<string>res(numRows,"");
+        int i=0;
+        int n=s.size();
+        while(i<n){
+            for(int j=0;j<numRows && i<n;j++){
+                res[j].push_back(s[i++]);   
+            }
+             
+            for(int j=numRows-2;j>=1&&i<n;j--){
+                res[j].push_back(s[i++]);
+            }
+        }
+        string ans ="";
+        for(auto it: res){
+            ans+=it;
+        }
+        return ans;
+    }
+};
